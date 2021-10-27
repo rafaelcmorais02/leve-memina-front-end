89c16e98fcc603700de2cf525af1d4df54c54c74
rafaelmoraiscunha <rafael.morais@franq.com.br>
2021-10-27 20:55:08 -0300

    [FEATURE] ajuste de layout e adicionar descriçao e titulo para os itens

--- src/data/dataCard.js
+++ src/data/dataCard.js
@@ -11,7 +11,7 @@ export const dadosCard = [
         current: "true",
         label: "Slide 1",
         imag: imag4,
-        titulo: "Conjunto Amarelo",
+        titulo: "1 - Conjunto Amarelo",
         descricao: "Alças reguláveis, todo detalhado em renda. Para um look sensual. Disponível no tamanho M"
     },
     {
@@ -19,92 +19,16 @@ export const dadosCard = [
         className: "carousel-item",
         label: "Slide 2",
         imag: imag2,
-        titulo: "titulo2",
-        descricao: "teste"
-
+        titulo: "2 - Conjunto Marrom",
+        descricao: "Alças reguláveis, todo detalhado em renda. Para um look sensual. Disponível no tamanho M"
     },
     {
         id: 2,
         className: "carousel-item",
         label: "Slide 3",
         imag: imag3,
-        titulo: "titulo3",
-        descricao: "teste"
+        titulo: "3 - Conjunto Vermelho",
+        descricao: "Alças reguláveis, todo detalhado em renda. Para um look sensual. Disponível no tamanho M"
 
-    },
-    {
-        id: 3,
-        className: "carousel-item",
-        label: "Slide 4",
-        imag: imag1,
-        titulo: "titulo4",
-        descricao: "teste"
-
-    },
-    {
-        id: 4,
-        className: "carousel-item",
-        label: "Slide 5",
-        imag: imag2,
-        titulo: "titulo5",
-        descricao: "teste"
-
-    },
-    {
-        id: 5,
-        className: "carousel-item",
-        label: "Slide 6",
-        imag: imag3,
-        titulo: "titulo6",
-        descricao: "teste"
-
-    },
-    {
-        id: 6,
-        className: "carousel-item",
-        label: "Slide 7",
-        imag: imag1,
-        titulo: "teste",
-        descricao: "teste"
-    },
-    {
-        id: 7,
-        className: "carousel-item",
-        label: "Slide 8",
-        imag: imag2,
-        titulo: "teste",
-        descricao: "teste"
-    },
-    {
-        id: 8,
-        className: "carousel-item",
-        label: "Slide 9",
-        imag: imag3,
-        titulo: "teste",
-        descricao: "teste"
-    },
-    {
-        id: 9,
-        className: "carousel-item",
-        label: "Slide 10",
-        imag: imag1,
-        titulo: "teste",
-        descricao: "teste"
-    },
-    {
-        id: 10,
-        className: "carousel-item",
-        label: "Slide 11",
-        imag: imag2,
-        titulo: "teste",
-        descricao: "teste"
-    },
-    {
-        id: 11,
-        className: "carousel-item",
-        label: "Slide 12",
-        imag: imag3,
-        titulo: "teste",
-        descricao: "teste"
-    },
+    }
 ];
